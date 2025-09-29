315f2bd5828e34c4aa0f9683a68f0330e7381e06
JashLadhani07 <jashladhani07@gmail.com>
2025-09-29 14:50:56 +0530

    Initial clean commit

---
+++ api/pydantic_models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from enum import Enum
+from datetime import datetime
+
+class ModelName(str, Enum):
+    GEMINI_25_PRO = "gemini-2.5-pro"
+    GEMINI_25_FLASH = "gemimi-2.5-flash"
+
+class QueryInput(BaseModel):
+    question: str
+    session_id: str = Field(default=None)
+    model: ModelName = Field(default=ModelName.GEMINI_25_PRO)
+
+class QueryResponse(BaseModel):
+    answer: str
+    session_id: str
+    model: ModelName
+
+class DocumentInfo(BaseModel):
+    id: int
+    filename: str
+    upload_timestamp: datetime
+
+class DeleteFileRequest(BaseModel):
+    file_id: int
+
